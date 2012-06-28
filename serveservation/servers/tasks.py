9968bc14e6015416f71709844d5ab4cb4f9dc2af
ddropinski <dropinskid@gmail.com>
2012-06-28 15:14:07 -0500

    Add email and periodic task support (using celery)

---
+++ serveservation/servers/tasks.py
@@ -0,0 +1,9 @@
+from celery.task.schedules import crontab
+from celery.decorators import periodic_task
+from servers.models import Reservation, check_expired
+
+@periodic_task(run_every=crontab(hour="*", minute="*", day_of_week="*"))
+def check_reservations():
+  all_reservations = Reservation.objects.all()
+  for res in all_reservations:
+    check_expired(res) 
