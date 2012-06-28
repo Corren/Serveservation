ffeb042a9ae808f440165658414a852a26f101c1
ddropinski <dropinskid@gmail.com>
2012-06-28 09:17:11 -0500

    Remove MEDIA_ROOT. Won't have user uploaded content to store.

--- serveservation/serveservation/settings.py
+++ serveservation/serveservation/settings.py
@@ -52,12 +52,12 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, "media" + os.path.sep)
+#MEDIA_ROOT = os.path.join(PROJECT_ROOT, "media" + os.path.sep)
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/s/media/'
+#MEDIA_URL = '/s/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
