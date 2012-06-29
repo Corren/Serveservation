016ade51233e31ee86f8bedc7610e1b4ea697bad
David Dropinski <DropinskiD@gmail.com>
2012-06-29 16:32:01 -0500

    Add individual server page. Also add links on full server list to individual servers.

--- serveservation/servers/server-urls.py
+++ serveservation/servers/server-urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('servers.views',
 
-  url(r'^$', 'servers'),
+  url(r'^$', 'all_servers'),
+  url(r'^(?P<servername>\w+)/$', 'server_page'),
 )
--- serveservation/servers/views.py
+++ serveservation/servers/views.py
@@ -1,13 +1,18 @@
 # Create your views here.
 #from django.template import Context, loader
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse
 from django.core.context_processors import csrf
 from django.forms.models import modelformset_factory
 from servers.models import Server, Reservation
 from servers.forms import ServerForm, ReservationForm
 
-def servers(request):
+def server_page(request, servername):
+  server = Server.objects.get(name=servername)
+  reservations = Reservation.objects.filter(server=server).exclude(reserved_by=None)
+  return render_to_response('servers/server.html', { 'server' : server, 'reservations' : reservations })
+
+def all_servers(request):
   all_servers_list = Server.objects.all()
   return render_to_response('servers/index.html', {'all_servers_list' : all_servers_list} )
 
