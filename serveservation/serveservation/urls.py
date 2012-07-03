6ee559f76e7e1182c5ad5c070216d6953eedaf8c
Jonathan <jonathanmgregory@hotmail.com>
2012-07-03 14:08:22 -0500

    Interface working better
    moved some of the files around and got the initial page to show up with
    template. Also added a 404 page.

--- serveservation/serveservation/urls.py
+++ serveservation/serveservation/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import patterns, include, url
+#from django.conf.urls.defaults import *#jonathan stuff
+#from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
+#handler404 = 'site_utils.handler404'
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'serveservation.views.home', name='home'),
@@ -16,9 +19,11 @@ urlpatterns = patterns('',
     url(r'^servers/', include('servers.server-urls')),
     url(r'^reservations/', include('servers.reservation-urls')),
     url(r'^accounts/', include('registration.backends.default.urls')),
+#    url(r'^login/$', include('auth.views.login_user')), #jonathan stuff
 )
 
 urlpatterns += patterns('django.views.generic.simple',
     (r'^accounts/profile/$', 'redirect_to', {'url': 'generic_account_url'}),
 )
 
+#urlpatterns += staticfiles_urlpatterns()
--- serveservation/serveservation/views.py
+++ serveservation/serveservation/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.core.context_processors import csrf
 from servers.models import Server
+#from django.http import Http404 #jg
+#from django.shortcuts import render_to_response, get_object_or_404
 
 #def index(request):
   #all_servers_list = Server.objects.all()
@@ -15,3 +17,10 @@ from servers.models import Server
 
 def home(request):
   return render_to_response('index.html' )
+  
+def detail(request, poll_id):
+    try:
+        p = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render_to_response('serveservation/error404.html', {'serveservation': p})
